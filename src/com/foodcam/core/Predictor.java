957a7105ce096e417724bdac23def65aaa5fa93f
root <root@localhost.localdomain>
2019-03-08 19:21:16 +0900

    Predictor 불필요 함수 제거, 주석수정

--- src/com/foodcam/core/Predictor.java
+++ src/com/foodcam/core/Predictor.java
@@ -18,10 +18,12 @@ import com.foodcam.util.pRes;
 public final class Predictor extends PredictorOperator {
 
 	private static class Singleton {
+		
 		static final Predictor INSTANCE = new Predictor();
 	}
 
 	public static Predictor getInstance() {
+		
 		return Singleton.INSTANCE;
 	}
 
@@ -32,11 +34,8 @@ public final class Predictor extends PredictorOperator {
 	 * @return
 	 */
 	public JSONObject predict(Mat receivedImg) {
+		
 		DataSet requestDataSet = getDataSetOfReceivedImg(receivedImg);
-
-//		String foodName = getFoodName(requestDataSet);
-//		String foodLink = getFoodLink(foodName);
-//		String firstLink = getFirstResponseFoodLink(requestDataSet);
 		
 		ArrayList<String> foodLinkListByTotalRanking = getFoodLinkListByTotalRanking(requestDataSet);
 		JSONObject json = new JSONObject();
@@ -45,12 +44,6 @@ public final class Predictor extends PredictorOperator {
 			json.put(Integer.toString(i + 1), foodLinkListByTotalRanking.get(i));
 		
 		return json;
-		
-
-//		pRes.log(foodName);
-//		pRes.log(foodLink);
-
-//		return getResult(foodLink);
 	}
 
 	/**
@@ -61,48 +54,10 @@ public final class Predictor extends PredictorOperator {
 	 * @return
 	 */
 	private DataSet getDataSetOfReceivedImg(Mat receivedImg) {
+		
 		DataSetLoader dataSetLoader = new DataSetLoader();
 		DataSet dataSet = dataSetLoader.getRequestDataSet(receivedImg);
 
 		return dataSet;
-	}
-
-	/**
-	 * 로드된 DataSet을 이용해 해당 이미지가 어떤 이미지인지 예측한다 예측 방법에는 k-NN 분류, descriptorMatching,
-	 * histogramComparing이 이용된다
-	 * 
-	 * @param requestDataSet
-	 * @return
-	 */
-	private String getFoodName(DataSet requestDataSet) {
-		int response = (int) knn.findNearest(requestDataSet.getFeatureVector().row(0), k, new Mat());
-		return responseMap.get(response);
-	}
-
-	/**
-	 * 예측된 음식이름을 이용해 해당 음식을 소개하는 웹링크를 받아온다
-	 * 
-	 * @param foodName
-	 * @return
-	 */
-	private String getFoodLink(String foodName) {
-		return linkMap.get(foodName);
-	}
-
-	/**
-	 * 링크를 받아온 후 최종적인 JSONObject형 리턴을 위해 해당 링크를 JSONObject에 담아 리턴한다
-	 * 
-	 * @param foodLink
-	 * @return
-	 */
-	private JSONObject getResult(String foodLink) {
-		JSONObject json = new JSONObject();
-		try {
-			json.put("link", foodLink);
-			return json;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
 	}
 }
--- src/com/foodcam/core/PredictorOperator.java
+++ src/com/foodcam/core/PredictorOperator.java
@@ -15,6 +15,13 @@ import com.foodcam.domain.DataSet;
  */
 public class PredictorOperator extends PredictorInitializer {
 
+	/**
+	 * 가장 유사한 음식들의 소개링크를 순위대로 순차저장해 리턴한다
+	 * ex) .get(0) : 1순위 음식의 소개링크
+	 * 
+	 * @param requestDataSet
+	 * @return
+	 */
 	ArrayList<String> getFoodLinkListByTotalRanking(DataSet requestDataSet) {
 
 		ArrayList<String> foodLinkListByTotalRanking = new ArrayList<>();
@@ -27,11 +34,23 @@ public class PredictorOperator extends PredictorInitializer {
 		return foodLinkListByTotalRanking;
 	}
 
+	/**
+	 * 가장 비슷한 1순위 음식의 소개링크를 받아온다
+	 * 
+	 * @param requestDataSet
+	 * @return
+	 */
 	private String getFirstResponseFoodLink(DataSet requestDataSet) {
 
 		return linkMap.get(responseMap.get(getFirstResponse(requestDataSet)));
 	}
 
+	/**
+	 * 가장 비슷한 1순위 음식의 knn response를 받아온다
+	 * 
+	 * @param requestDataSet
+	 * @return
+	 */
 	private int getFirstResponse(DataSet requestDataSet) {
 
 		return (int) knn.findNearest(requestDataSet.getFeatureVector().row(0), k, new Mat());
