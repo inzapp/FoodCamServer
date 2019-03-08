07cee6c4f68917b74775bc32bbe0819b6d876f9b
root <root@localhost.localdomain>
2019-03-08 18:47:37 +0900

    knn을 이용한 단일 순위 백업

--- src/com/foodcam/core/PredictorOperator.java
+++ src/com/foodcam/core/PredictorOperator.java
@@ -20,12 +20,11 @@ public class PredictorOperator extends PredictorInitializer {
 		ArrayList<String> foodLinkListByTotalRanking = new ArrayList<>();
 		foodLinkListByTotalRanking.add(getFirstResponseFoodLink(requestDataSet));
 
-		ArrayList<String> foodLinkListExceptFirstRanking = getFoodLinkListExceptFirstRanking(requestDataSet);
-		for (String curExceptFirstRanking : foodLinkListExceptFirstRanking)
-			foodLinkListByTotalRanking.add(curExceptFirstRanking);
+//		ArrayList<String> foodLinkListExceptFirstRanking = getFoodLinkListExceptFirstRanking(requestDataSet);
+//		for (String curExceptFirstRanking : foodLinkListExceptFirstRanking)
+//			foodLinkListByTotalRanking.add(curExceptFirstRanking);
 
 		return foodLinkListByTotalRanking;
-
 	}
 
 	private String getFirstResponseFoodLink(DataSet requestDataSet) {
@@ -38,24 +37,29 @@ public class PredictorOperator extends PredictorInitializer {
 		return (int) knn.findNearest(requestDataSet.getFeatureVector().row(0), k, new Mat());
 	}
 
-	private ArrayList<String> getFoodLinkListExceptFirstRanking(DataSet requestDataSet) {
-
-		ArrayList<String> foodLinkListExceptFirstRanking = new ArrayList<>();
-		ArrayList<Mat> requestHistogramList = requestDataSet.getHistogramList();
-		Mat requestHistogram = requestHistogramList.get(0);
-		
-		double m0Res, m1Res, m2Res, m3Res, t1Res, t2Res, t3Res, t4Res;
-		m0Res = m1Res = m2Res = m3Res = -1;
-		
-		
-		for(Mat curServerHistogram : this.histogramList) {
-			m0Res = Imgproc.compareHist(curServerHistogram, requestHistogram, Imgproc.HISTCMP_CORREL);
-			m1Res = Imgproc.compareHist(curServerHistogram, requestHistogram, Imgproc.HISTCMP_CHISQR);
-			m2Res = Imgproc.compareHist(curServerHistogram, requestHistogram, Imgproc.HISTCMP_INTERSECT);
-			m3Res = Imgproc.compareHist(curServerHistogram, requestHistogram, Imgproc.HISTCMP_HELLINGER);
-		}
-
-		return foodLinkListExceptFirstRanking;
-	}
+//	private ArrayList<String> getFoodLinkListExceptFirstRanking(DataSet requestDataSet) {
+//
+//		ArrayList<String> foodLinkListExceptFirstRanking = new ArrayList<>();
+//		ArrayList<Mat> requestHistogramList = requestDataSet.getHistogramList();
+//		Mat requestHistogram = requestHistogramList.get(0);
+//		
+////		double m0Res, m1Res, m2Res, m3Res, t0Res, t1Res, t2Res, t3Res;
+////		m0Res = m2Res = t0Res = t2Res = -1;
+////		m1Res = m1Res = t0Res = t2Res = Double.MAX_VALUE;
+//		
+//		double bestCompareResult, curCompareResult;
+//		bestCompareResult = curCompareResult = -1;
+//		Mat bestHistogram = new Mat();
+//		
+//		for(Mat curServerHistogram : this.histogramList) {
+//			curCompareResult = Imgproc.compareHist(curServerHistogram, requestHistogram, Imgproc.HISTCMP_INTERSECT);
+//			if(curCompareResult < bestCompareResult) {
+//				bestHistogram = curServerHistogram;
+//				bestCompareResult = curCompareResult;
+//			}
+//		}
+//
+//		return foodLinkListExceptFirstRanking;
+//	}
 
 }
