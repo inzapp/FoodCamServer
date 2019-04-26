26ae970baf83b06d030eaeec33bbb31d9041ce60
inzapp <inzapp@naver.com>
2019-04-26 16:21:56 +0900

    윈도우 마이그레이션

--- src/com/foodcam/ServerInitializer.java
+++ src/com/foodcam/ServerInitializer.java
@@ -20,8 +20,7 @@ import com.foodcam.util.pRes;
 class ServerInitializer {
 	
 	static {
-		// System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // for STS
-		System.load("/inz/lib/libopencv_java401.so");
+		System.load("C:\\inz\\lib\\opencv_java410.dll");
 	}
 
 	/**
